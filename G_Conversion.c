1dbfbc22f8031db56e8f99d6be76ff6a0b9ed627
freelancersifat627e <freelancersifat627@gmail.comm>
2025-10-25 13:17:24 +0600

    code clean on probelm G

--- G_Conversion.c
+++ G_Conversion.c
@@ -1,52 +1,25 @@
 #include<stdio.h>
 int main(){
     char s[100001];
-    scanf("%s" , &s);
-    for(int i=0; i<s[i]; i++){
-        // printf("%c" , s[i]);
-        // !==============================
 
-          if(s[i]== ','){
+    scanf("%s" , &s);
+
+    for(int i=0; i<s[i]; i++){
+    //    Remove , & add space
+        if(s[i]== ','){
             s[i] = ' ';
             printf("%c",s[i]);
         }
 
-
         if(s[i] >= 97 && s[i] <= 122){
-            s[i] -= 32;
+            s[i] -= 32; // convert lowercase to uppercase
             printf("%c" , s[i]);
         }else if(s[i] >= 65 && s[i] <= 90){
-            s[i] += 32;
+            s[i] += 32; // convert uppercase to  lowercase
             printf("%c" , s[i]);
         }
-        // !=============================
-
-        // if(s[i]== ','){
-        //     s[i] = ' ';
-        //     // printf("%c",s[i]);
-        //     if(s[i] >= 97 && s[i] <= 122){
-        //         s[i] -= 32;
-        //         printf("%c" , s[i]);
-        //     }else if(s[i] >= 65 && s[i] <= 90){
-        //         s[i] += 32;
-        //         printf("%c" , s[i]);
-        //     }
-        // }
-
-        //   if(s[i] >= 97 && s[i] <= 122){
-        //     s[i] -= 32;
-        //     printf("%c" , s[i]);
-        // }else if(s[i] >= 65 && s[i] <= 90){
-        //     s[i] += 32;
-        //     printf("%c" , s[i]);
-        // }
-
-        // !==============================
-        // if(s[i]== ','){
-        //     s[i] = ' ';
-        //     printf("%c",s[i]);
-        // }
+               
     }
-    // printf("%s" , s);
+    
     return 0;
 }
